e0cf70c21dad0438764dc283ffa3c946508621c6
EvgeniyBochevarov <bochevarov@gmail.com>
2025-03-09 12:15:54 +0200

    Урок1. Задание 5

---
+++ lib/Lesson1/Task5.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+void main() {
+  print('Калькулятор');
+  print('Введіть перше число: ');
+  double? shislo1 = double.tryParse(stdin.readLineSync()!);
+
+  print('Введіть друге число: ');
+  double? shislo2 = double.tryParse(stdin.readLineSync()!);
+  print('Оберіть операцію (+, -, *, /)');
+  String? oper = stdin.readLineSync()!;
+
+  switch (oper) {
+    case '+':
+      print('Результат: $shislo1 $oper $shislo2 = ${add(shislo1!, shislo2!)}');
+      break;
+    case '-':
+      print('Результат: $shislo1 $oper $shislo2 = ${subtract(shislo1!, shislo2!)}');
+      break;
+    case '*':
+      print('Результат: $shislo1 $oper $shislo2 = ${multiply(shislo1!, shislo2!)}');
+      break;
+    case '/':
+      print('Результат: $shislo1 $oper $shislo2 = ${divide(shislo1!, shislo2!)}');
+      break;
+    default:
+      print('Невірна операція. Спробуйте знову.');
+  }
+
+
+}
+
+double add(double ch1, double ch2) {
+  return ch1 + ch2;
+}
+
+double subtract(double ch1, double ch2) {
+  return ch1 - ch2;
+}
+
+double multiply(double ch1, double ch2) {
+  return ch1 * ch2;
+}
+
+double divide(double ch1, double ch2) {
+  return ch1 / ch2;
+}
