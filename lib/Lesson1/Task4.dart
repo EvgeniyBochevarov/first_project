abc65386fb9a31c08cf0b6cbf1862c7d60db5516
EvgeniyBochevarov <bochevarov@gmail.com>
2025-03-09 11:20:24 +0200

    Урок1. Задание 4

---
+++ lib/Lesson1/Task4.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+void main() {
+  print('Конвертер часу');
+  print('Введіть значення часу: ');
+  double? vrem = double.tryParse(stdin.readLineSync()!);
+
+  if (vrem != null) {
+    print('Введіть цифру варіант конвертації: ');
+    var textV = '''
+    1. Години в хвилини
+    2. Години в секунди
+    3. Мінути в години
+    4. Мінути в секунди''';
+    print(textV);
+    int? choice = int.tryParse(stdin.readLineSync()!);
+
+    switch (choice) {
+      case 1:
+        print('Результат: ${hoursToMinutes(vrem)} хвилин');
+        break;
+      case 2:
+        print('Результат: ${hoursToSeconds(vrem)} секунд');
+        break;
+      case 3:
+        print('Результат: ${minutesToHours(vrem)} годин');
+        break;
+      case 4:
+        print('Результат: ${minutesToSeconds(vrem)} секунд');
+        break;
+      default:
+        print('Невірний вибір. Спробуйте знову.');
+    }
+
+  } else {
+    print('Введіть корректне число.');
+  }
+}
+
+double hoursToMinutes(double hours) {
+  return hours * 60;
+}
+
+double hoursToSeconds(double hours) {
+  return hours * 3600;
+}
+
+double minutesToHours(double minutes) {
+  return minutes / 60;
+}
+
+double minutesToSeconds(double minutes) {
+  return minutes * 60;
+}
